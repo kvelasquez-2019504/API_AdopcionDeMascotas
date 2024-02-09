dfd41fea4aaa71e23b3413211b3c157cfb2c1b87
Kenneth Velasquez <kvelasquez-2019504@kinal.edu.gt>
2024-02-09 08:58:07 -0600

    Se agrego la ruta de mascotas

--- controllers/mascota.controller.js
+++ controllers/mascota.controller.js
@@ -2,6 +2,19 @@ const {response}= require('express');
 const Mascota = require('../models/mascota');
 
 const mascotasGet=async (req,res=response)=>{
-    const {limites, desde} = req.query;
+    const {limite, desde} = req.query;
     const query={estado:true}
+    const [total,mascotas]=await Promise.all([
+        Mascota.countDocuments(query),
+        Mascota.find(query)
+        .skip(Number(desde))
+        .limit(Number(limite))
+    ]);
+    res.status(200).json({
+        total, mascotas
+    });
+}
+
+module.exports = {
+    mascotasGet
 }
--- models/mascota.js
+++ models/mascota.js
@@ -2,15 +2,26 @@ const {Schema,module}=require("mongoose");
 const MascotaSchema = Schema[{
     nombre:{
         type:String,
-        required:[true,"El nombre es obligatorio"]
+        required:[true,"El nombre de tu mascota es obligatorio"]
+    },
+    tipo:{
+        type:String,
+        required:[true, "El tipo de mascota es obligatorio"]
     },
     raza:{
-        type:String,
-        required:[true,"La raza es obligatoria"]
+        type:String
     },
     edad:{
         type:Integer,
-        required:[true,"La edad es obligatoria"]
+        required:[true,"La edad de tu mascota es obligatoria"]
+    },
+    peso:{
+        type:Double,
+        required:[true,"El peso de tu mascota es obligatorio"]
+    },
+    altura:{
+        type:Double,
+        required:[true,"La altura de tu mascota es obligatoria"]
     },
     estado:{
         type:Boolean,
